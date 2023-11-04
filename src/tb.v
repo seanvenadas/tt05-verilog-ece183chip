343d93a00c2e3f139789776187a38863a2cd3deb
seanvenadas <149636050+seanvenadas@users.noreply.github.com>
2023-11-03 22:37:37 -0700

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_seanvenadas tt_um_seven_segment_seconds (
+    tt_um_seanvenadas tt_um_seanvenadas (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
